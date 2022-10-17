10cbca83bd1b37a38bfb3af134ef463885d88fd2
mbasadi <asadi.mohammadbagher@gmail.com>
2022-10-17 12:51:36 -0230

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,14 @@
+const { cdk } = require('projen');
+const project = new cdk.JsiiProject({
+  author: 'mbasadi',
+  authorAddress: 'asadi.mohammadbagher@gmail.com',
+  defaultReleaseBranch: 'main',
+  name: 'jsii-time-ago',
+  repositoryUrl: 'https://github.com/mbasadi/jsii-time-ago.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
